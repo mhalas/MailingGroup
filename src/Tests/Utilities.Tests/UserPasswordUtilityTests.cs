107194c5ba11c8010174ccfff6a561ec9bb91d02
Maciej Hałas <maciejhalas@outlook.com>
2021-03-13 14:37:31 +0100

    Add Utility tests for Email address validator and for User Password.

--- src/Tests/Utilities.Tests/UserPasswordUtilityTests.cs
+++ src/Tests/Utilities.Tests/UserPasswordUtilityTests.cs
@@ -1,22 +1,52 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Utilities.Tests
 {
     [TestFixture]
     public class UserPasswordUtilityTests
     {
-        [Test]
-        public void TestMethod()
+        [TestCase("TestPassword", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword1", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("BestPasswordGeneratorEver", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
+        public void Should_ReturnFalse_when_CheckingPasswordCorrectnessOnDifferentPassword(string password, string hashedDifferentPassword)
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
+            var saltGenerator = new SaltGeneratorUtility();
+
+            var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
+
+            var isPasswordNotTheSameAsOriginPassword = userPasswordUtility.IsPasswordCorrect(password, salt, hashedDifferentPassword);
+
+            Assert.IsFalse(isPasswordNotTheSameAsOriginPassword);
+        }
+
+        [TestCase("TestPassword", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword1", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
+        public void Should_ReturnTrue_when_CheckingPasswordCorrectness(string password, string hashedPassword)
+        {
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
+
+            var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
+
+            var isPasswordTheSameAsOriginPassword = userPasswordUtility.IsPasswordCorrect(password, salt, hashedPassword);
+
+            Assert.IsTrue(isPasswordTheSameAsOriginPassword);
+        }
+
+        [TestCase("TestPassword", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword1", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
+        public void Should_ReturnHashedPassword_when_PassUnhashedPassword(string password, string correctHashedPassword)
+        {
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
+            var saltGenerator = new SaltGeneratorUtility();
+
+            var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
+            var hashedPassword = userPasswordUtility.HashPassword(password, salt);
+
+            Assert.AreEqual(correctHashedPassword, hashedPassword);
         }
     }
 }
--- src/Utilities/EmailAddressValidatorUtility.cs
+++ src/Utilities/EmailAddressValidatorUtility.cs
@@ -5,6 +5,10 @@ namespace Utilities
     /// <summary>
     /// I could use regex for validate email addresses, but 100% currect regex is unreadable.
     /// http://www.ex-parrot.com/pdw/Mail-RFC822-Address.html
+    /// 
+    /// Also:
+    /// https://haacked.com/archive/2007/08/21/i-knew-how-to-validate-an-email-address-until-i.aspx/
+    /// https://en.wikipedia.org/wiki/Email_address#Local-part
     /// </summary>
     public class EmailAddressValidatorUtility
     {
