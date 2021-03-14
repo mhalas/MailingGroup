aae630b17d4eb1c5824194783da35efb6b2ebf74
Maciej Hałas <maciejhalas@outlook.com>
2021-03-15 00:29:19 +0100

    Hotfix - change hash password method - now using Pbkdf2.

--- src/Tests/Utilities.Tests/UserPasswordUtilityTests.cs
+++ src/Tests/Utilities.Tests/UserPasswordUtilityTests.cs
@@ -6,13 +6,12 @@ namespace Utilities.Tests
     [TestFixture]
     public class UserPasswordUtilityTests
     {
-        [TestCase("TestPassword", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("TestPassword1", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("BestPasswordGeneratorEver", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword", "vUT2c37jiiA=")]
+        [TestCase("TestPassword1", "jHCaNisYhos=")]
+        [TestCase("BestPasswordGeneratorEver", "qg4PMFZIa+g=")]
         public void Should_ReturnFalse_when_CheckingPasswordCorrectnessOnDifferentPassword(string password, string hashedDifferentPassword)
         {
-            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
-            var saltGenerator = new SaltGeneratorUtility();
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 8);
 
             var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
 
@@ -21,12 +20,12 @@ namespace Utilities.Tests
             Assert.IsFalse(isPasswordNotTheSameAsOriginPassword);
         }
 
-        [TestCase("TestPassword", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("TestPassword1", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword", "jHCaNisYhos=")]
+        [TestCase("TestPassword1", "qg4PMFZIa+g=")]
+        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiA=")]
         public void Should_ReturnTrue_when_CheckingPasswordCorrectness(string password, string hashedPassword)
         {
-            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 8);
 
             var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
 
@@ -35,13 +34,12 @@ namespace Utilities.Tests
             Assert.IsTrue(isPasswordTheSameAsOriginPassword);
         }
 
-        [TestCase("TestPassword", "jHCaNisYhougdE3a+LMZ+w==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("TestPassword1", "qg4PMFZIa+jBZxiTn9+hQQ==VXNlclBhc3N3b3JkVGVzdA==")]
-        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiBD07HIdPykZA==VXNlclBhc3N3b3JkVGVzdA==")]
+        [TestCase("TestPassword", "jHCaNisYhos=")]
+        [TestCase("TestPassword1", "qg4PMFZIa+g=")]
+        [TestCase("BestPasswordGeneratorEver", "vUT2c37jiiA=")]
         public void Should_ReturnHashedPassword_when_PassUnhashedPassword(string password, string correctHashedPassword)
         {
-            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 16);
-            var saltGenerator = new SaltGeneratorUtility();
+            var userPasswordUtility = new UserPasswordUtility(iterations: 1, keySize: 8);
 
             var salt = Encoding.ASCII.GetBytes("UserPasswordTest");
             var hashedPassword = userPasswordUtility.HashPassword(password, salt);
